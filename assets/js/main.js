03f965c3b6fc5e7e16b68d331f9d5beed6e26a92
Romina Trazzi <romina.trazzi@posta.it>
2020-11-12 13:09:36 +0100

    Commit final
    Esercizio Javascript password generator

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+/*=============================================
+    =           JAVASCRIPT CODE          =
+=============================================*/
+
+
+
+var name = prompt("Come ti chiami?");
+
+alert("Ciao " + name);
+
+var age = prompt("Quanti anni hai?");
+var color = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("result").innerHTML = name + age + color + "20";
+
+
+// Forma breve 
+
+// result.innerHTML = name + age + color + "20";
+
+
+
